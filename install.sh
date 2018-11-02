7dfd72b384c98ef61417e0e447be8f5bdf3eb97c
EdmundGoodman <37504168+EdmundGoodman@users.noreply.github.com>
2018-11-02 22:10:39 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@ echo "Installing pre-requisite libraries"
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python-opencv
-#pip install -U scikit-learn
-pip install pillow
-pip install matplotlib
-
-
 sudo apt-get install xboxdrv
+pip install pillow
+pip install numpy
