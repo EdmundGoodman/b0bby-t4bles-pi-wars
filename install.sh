5ed0092d8d446749aec13b04c213a0d8fcad1f08
EdmundGoodman <37504168+EdmundGoodman@users.noreply.github.com>
2018-11-02 21:28:21 +0000

    Added pillow, removed sci-kit

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ echo "Installing pre-requisite libraries"
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python-opencv
-pip install -U scikit-learn
+#pip install -U scikit-learn
+pip install pillow
 pip install matplotlib
 
 
