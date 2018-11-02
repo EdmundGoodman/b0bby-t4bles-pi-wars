31815c0c200990ea4dddfc1361791e5d26f767e9
EdmundGoodman <37504168+EdmundGoodman@users.noreply.github.com>
2018-11-02 21:25:19 +0000

    Update images.py

--- images/images.py
+++ images/images.py
@@ -1,57 +1,11 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-
+from collections import Counter
+from PIL import Image
 
 class image_classifier:
-    def __init__(self, NO_CLUSTERS, DISTANCE_THRESHOLD):
-        self.no_clusters = NO_CLUSTERS
-        self.distance_threshold = DISTANCE_THRESHOLD
-
-
-    def draw_histogram(self, hist, clt):
-        #Draw the histogram
-        bar = self.plot_colors2(hist, clt.cluster_centers_)
-        plt.axis("off")
-        plt.imshow(bar)
-        plt.show()
-
-
-    def find_histogram(self, clt):
-        numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-        (hist, _) = np.histogram(clt.labels_, bins=numLabels)
-        hist = hist.astype("float")
-        hist /= hist.sum()
-        return hist
-
-
-    def plot_colors2(self, hist, centroids):
-        bar = np.zeros((50, 300, 3), dtype="uint8")
-        startX = 0
-        for (percent, color) in zip(hist, centroids):
-            # plot the relative percentage of each cluster
-            endX = startX + (percent * 300)
-            cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-                          color.astype("uint8").tolist(), -1)
-            startX = endX
-        return bar
-
-
-    def cluster_colours(self, img):
-        #Cluster the image into the $NO_CLUSTERS most prevalent colours, using the KMeans class
-        print("Clustering colours")
-        clt = KMeans(n_clusters=self.no_clusters)
-        clt.fit(img)
-        return clt
-
-
-    def generate_histogram(self, clt):
-        #Generate and show a histogram of the $NO_CLUSTERS most prevalent colours
-        print("Generating histogram")
-        hist =self.find_histogram(clt)
-        return hist
-
+    def __init__(self, DISTANCE_THRESHOLD, NUMBER_PIXEL_THRESHOLD, ROUNDBASE):
+        self.roundBase = ROUNDBASE
+        self.distanceThreshold = DISTANCE_THRESHOLD
+        self.numberPixelThreshold = NUMBER_PIXEL_THRESHOLD
 
     def get_base_colour(self, rgb_tuple):
         def manhattan(x,y):
@@ -67,38 +21,34 @@ class image_classifier:
         }
         distances = {k: manhattan(v, rgb_tuple) for k, v in colours.items()}
         color = min(distances, key=distances.get)
-        return color, distances[color]
-
+        return color #, distances[color]
 
     def classify(self, img):
-        clt = self.cluster_colours(img)
-        hist = self.generate_histogram(clt)
-        self.draw_histogram(hist, clt)
+        #Reduce colour depth
+        img = img.point(lambda x: int(x/self.roundBase)*self.roundBase)
+        pixels = img.getdata()
+        #Count pixels & reject outliers
+        pixelsCount = Counter(pixels)
+        for k in list(pixelsCount):
+            if pixelsCount[k] < self.numberPixelThreshold:
+                del pixelsCount[k]
+        #Classify colour based on manhattan distance from component colours
+        colourValues = {"red": 0, "green": 0, "blue": 0, "yellow": 0}
+        for pixels,number in pixelsCount.items():
+            colour = self.get_base_colour(pixels)
+            if colour in colourValues.keys():
+                colourValues[colour] += number
+        #Return the most prevalent colour
+        colour = max(colourValues, key=colourValues.get)
+        return colour
 
-        predominantColour, maxIntensity = None, 0
-        for i in range(len(hist)):
-            cluster, intensity = clt.cluster_centers_[i], hist[i]
-            colour, distance = self.get_base_colour(cluster)
-            #print(cluster, colour, distance, intensity)
-            if colour != "white" and colour != "black" and distance < DISTANCE_THRESHOLD:
-                if intensity > maxIntensity:
-                    predominantColour, maxIntensity = colour, intensity
-        return predominantColour
-
-
-def load_image(IMAGE):
-    #Read the image, and switch the colour channels to RGB
-    print("Loading images")
-    img = cv2.imread(IMAGE)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
+def load_image(filePath):
+    #Read the image
+    img = Image.open(filePath)
     return img
 
-NO_CLUSTERS = 2
-DISTANCE_THRESHOLD = 255*2
-
-IMAGE = "redBlock.jpg"
-img = load_image(IMAGE)
-classifier = image_classifier(NO_CLUSTERS, DISTANCE_THRESHOLD)
-colour = classifier.classify(img)
-print(colour)
+filePath = "redBlock.png"
+img = load_image(filePath)
+DISTANCE_THRESHOLD, NUMBER_PIXEL_THRESHOLD, ROUNDBASE = 100,100,20
+classifier = image_classifier( DISTANCE_THRESHOLD, NUMBER_PIXEL_THRESHOLD, ROUNDBASE)
+print(classifier.classify(img))
